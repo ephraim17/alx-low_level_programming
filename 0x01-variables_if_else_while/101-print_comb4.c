522b90adcc9366de06153df130a1def1ebdc7300
ephraim17 <emulilo17@gmail.com>
2022-03-14 15:01:28 -0700

    added files for task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - task11
+ *
+ * Description: Print all possible different combinations of three leters
+ * Return: Always(0) Success
+ */
+
+int main(void)
+{
+	return (0);
+}
