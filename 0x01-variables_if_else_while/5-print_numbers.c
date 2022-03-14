62d6432bec48ce39817c1364df862d5dc3721c91
ephraim17 <emulilo17@gmail.com>
2022-03-14 10:54:23 -0700

    added solution 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *  main - Task
+ *
+ *  Description: Print numbers 1 to 10 followed by a new line
+ *  Return: Always(0) Success
+ */
+
+int main(void)
+{
+	int number = 0;
+
+	for (number = 0 ; number <= 10 ; number++)
+	{
+		printf("%d\n", number);
+	}
+
+	return (0);
+}
