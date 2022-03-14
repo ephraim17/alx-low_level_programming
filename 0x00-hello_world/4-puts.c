5552dd0cc4d187df9cbb3cc3dff4a608855dfb14
ephraim17 <emulilo17@gmail.com>
2022-03-14 15:08:59 -0700

    fixed betty for task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,12 @@
 
 /**
  * main - Entry point
- * 
+ *
  * Return: Always 0 (Success)
  */
 
-int main(void) {
-    puts("\"Programming is like building a multilingual puzzle");
-    return(0);
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
