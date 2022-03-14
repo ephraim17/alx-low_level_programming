2841fe7dfb44f67ac01f690ab9c3d0e9bebcdf60
ephraim17 <emulilo17@gmail.com>
2022-03-14 14:14:17 -0700

    added betty stles to solu 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,15 +5,15 @@
  * Always return(0)
  */
 
-int main (void) {
-   char ch;
+int main(void)
+{
+	char ch;
 
-   for(ch = 'a' ; ch <= 'z' ; ch++) 
-   {
-      putchar(ch);
-   }
-   putchar('\n');
-   
-   return(0);
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
 }
 
