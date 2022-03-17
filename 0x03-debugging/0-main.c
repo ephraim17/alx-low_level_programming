afe96cad8890e52067a4255da621e2c085228416
ephraim17 <emulilo17@gmail.com>
2022-03-17 05:12:10 -0700

    Fixed solution for task 1

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -tests function that prints if int is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 6;
+	positive_or_negative(i);
+
+	return (0);
+}
