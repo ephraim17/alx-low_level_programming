8f0091098931c3188f57874d4eba9aa296246905
ephraim17 <emulilo17@gmail.com>
2022-03-18 04:15:42 -0700

    Added solution for first task

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int _isupper(int c)
+	{
+		return (c >= 'A' && c <= 'Z');
+	}
+}
