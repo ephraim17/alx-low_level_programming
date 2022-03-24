258b488dfd59d3634e4a24f0cb0c794c9c5351c0
ephraim17 <emulilo17@gmail.com>
2022-03-24 16:16:29 -0700

    Added solution for task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - function to compare two strings
+ * @s1: point
+ *
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int flag = 0,  i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			flag = s1[i] - s2[i];
+			break;
+		}
+	}
+	return (flag);
+}
