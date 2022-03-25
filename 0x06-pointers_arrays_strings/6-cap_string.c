fed203a506b9a136482e0f7c2ee74f57edc91d57
ephraim17 <emulilo17@gmail.com>
2022-03-25 15:31:04 -0700

    Fixed betty styles

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -17,7 +17,7 @@ char *cap_string(char *s)
 		if (s[0] >= 97 && s[0] <= 122)
 			s[0] = s[0] - 32;
 		for (j = 0; delimeters[j] != '\0'; j++)
-			if (s[i] == delimeters[j] && s[i + 1] >= 97 && s[i +1] <= 122)
+			if (s[i] == delimeters[j] && s[i + 1] >= 97 && s[i + 1] <= 122)
 				s[i + 1] = s[i + 1] - 32;
 	}
 	return (s);
