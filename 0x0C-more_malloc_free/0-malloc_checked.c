7924959f442f450c3e016773310cdbff4f6612b8
ephraim17 <emulilo17@gmail.com>
2022-04-07 17:59:32 -0700

    Added files for task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ *  malloc_checked - allocates memory using malloc
+ *  @b: input character
+ *
+ *  Return: a pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *pointer;
+
+	pointer = malloc(b);
+	if (!pointer)
+	{
+		exit(98);
+	}
+	return (pointer);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,4 @@
+This project aims to help the student understand
+
+1) How to use the exit function
+2) What are the functions calloc and realloc from the stdlib and how to use them.
