16bd25a9ea936e1622d4251bdbf621ff55334770
ephraim17 <emulilo17@gmail.com>
2022-04-11 09:26:29 -0700

    test solution for task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * struct dog - dog structure
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: (0) Success.
+ */
+
+void init_dog(struct dog *d, char name, float age, char *owner)
+{
+	struct dog
+	{
+		char name;
+		float age;
+		char *owner;
+	} my_dog;
+}
