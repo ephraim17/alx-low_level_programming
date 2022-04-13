738a1abfd75ef813987a91b7f9bd1350b786741c
ephraim17 <emulilo17@gmail.com>
2022-04-13 12:24:57 -0700

    Added solution for task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: first param
+ * @size: second param
+ * @cmp: third param
+ *
+ * Return:(0) Always Success
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0 || array == NULL || cmp == NULL)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(*(array + i)))
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
+
