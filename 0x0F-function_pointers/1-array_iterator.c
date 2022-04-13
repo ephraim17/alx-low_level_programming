6df063811bbd164a480012ae4c0938c25e4d68eb
ephraim17 <emulilo17@gmail.com>
2022-04-13 12:17:14 -0700

    Attempt to fix task 1 bugs

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,6 @@
 #include "function_pointers.h"
 #include <stdio.h>
+#include <stddef.h>
 
 /**
  * array_iterator - execute a function given as a param on each elem of array
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
 #ifndef _function_pointers_
-#define _function_pointers_
+#define _function_pointers
+#include <stddef.h>
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
