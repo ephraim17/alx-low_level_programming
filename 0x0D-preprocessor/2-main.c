71e8028d6b4747bd5a6ae1381d188e7081df1225
ephraim17 <emulilo17@gmail.com>
2022-04-12 19:16:06 -0700

    Added files for task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print file name compiled from
+ *
+ * Return: (0) Success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
