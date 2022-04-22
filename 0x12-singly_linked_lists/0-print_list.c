785b9654fbbc4aaec8bcf69760ccd34096860fc9
ephraim17 <emulilo17@gmail.com>
2022-04-22 10:15:11 -0700

    Added files for task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * print_list - prints elements of list_t list
+ * @h: list_t
+ *
+ * Return: Number of nodes in h
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		if (!h->str)
+		{
+			printf("[0] (nil)\n");
+			return (0);
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+			h = h->next;
+			n++;
+		}
+	}
+	return (n);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,4 @@
+This project aims to help the student understand:
+
+1) When and why to use linked lists vs arrays
+2) How to build and use linked lists.
