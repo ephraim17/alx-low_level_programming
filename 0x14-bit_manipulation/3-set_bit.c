f5cfcbedff4cb29b1d2e41d8a4bf8d6d9a29ba29
ephraim17 <emulilo17@gmail.com>
2022-04-28 18:29:14 -0700

    Added files for task 3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * set_bit - sets value of nth bit to 1 at a given index
+ * @index: index number we're using
+ * @n: n
+ * Return: (1) or (-1)
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > sizeof(n) * 8 - 1)
+	{
+		return (-1);
+	}
+	return (((1 << index) | *n ));
+}
